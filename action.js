ae2d63086dfd4531f6ad80142c049d55ff609f89
Nayanjeet Saikia <65288687+Neyen108@users.noreply.github.com>
2021-02-07 04:58:15 +0530

    add automated updation of data (action.js)

---
+++ action.js
@@ -0,0 +1,65 @@
+const path = require('path');
+const https = require('https');
+const fs = require('fs');
+const csv = require('@fast-csv/parse');
+
+const countries = require('./data/countries.json');
+
+const pathToData = path.join(__dirname, 'data', 'spotifyWorldCharts.json');
+const pathToUrlData = path.join(__dirname, 'data', 'urlData.json');
+
+const getTopTracksPerCountry = async (country) => {
+  let data = [];
+
+  //return the promise
+
+  return new Promise((resolve, reject) => {
+    //get the csv file
+    https.get(
+      `https://spotifycharts.com/regional/${country}/daily/latest/download`,
+      (res) => {
+        csv
+          .parseStream(res, { skipRows: 2, maxRows: 10 })
+          .on('error', () => reject())
+          .on('data', (row) => {
+            const obj = {
+              number: row[0],
+              trackName: row[1],
+              artist: row[2],
+              streams: row[3],
+              url: row[4],
+            };
+
+            data = [...data, obj];
+          })
+          .on('end', () => {
+            resolve(data);
+          });
+      }
+    );
+  });
+};
+
+const getCountriesData = async () => {
+  const data = {};
+  const urlData = {};
+
+  for (const country of Object.keys(countries)) {
+    const countryData = await getTopTracksPerCountry(country);
+
+    if (countryData) {
+      data[country.toUpperCase()] = countryData;
+      console.log(countryData);
+
+      urlData[country.toUpperCase()] = countryData.map((item) => {
+        const lastItem = item.url.substring(item.url.lastIndexOf('/') + 1);
+        return lastItem;
+      });
+    }
+  }
+
+  fs.writeFileSync(pathToData, JSON.stringify(data));
+  fs.writeFileSync(pathToUrlData, JSON.stringify(urlData));
+};
+
+getCountriesData();
