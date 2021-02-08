5b9099da4f1fc626a6b886222cfeceac886afb6d
Nayanjeet Saikia <65288687+Neyen108@users.noreply.github.com>
2021-02-08 15:52:31 +0530

    add world and MapChart

---
+++ src/Loader.js
@@ -0,0 +1,11 @@
+import './Loader.css';
+
+const Loader = () => {
+  return (
+    <div className='Container'>
+      <div className='Loader'></div>
+    </div>
+  );
+};
+
+export default Loader;
---
+++ src/World.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import ReactTooltip from 'react-tooltip';
+
+import MapChart from './MapChart';
+import './World.css';
+
+const World = () => {
+  const [content, setContent] = useState('');
+
+  return (
+    <div className='World'>
+      <h1 className='Title'>
+        Spotify <span>Top Tracks</span> by Region
+      </h1>
+      <div className='Subtitle'>
+        Hover on a country to see the most streamed track of the day
+      </div>
+      <MapChart setTooltipContent={setContent} />
+      <ReactTooltip>{content}</ReactTooltip>
+    </div>
+  );
+};
+
+export default World;
