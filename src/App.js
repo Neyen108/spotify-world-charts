f12aeca01cc7bce80130b80ce613f2ab6061d4f5
Nayanjeet Saikia <65288687+Neyen108@users.noreply.github.com>
2021-02-08 19:18:02 +0530

    add react-query

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import './App.css';
 import React, { useState } from 'react';
+import { useQuery } from 'react-query';
 
 import World from './World';
 import Loader from './Loader';
 
 function App() {
-  return (
-    <>
-      <World />
-    </>
-  );
+  //get the countries from my repository
+  const { isLoading, data } = useQuery('repoData', () => {
+    fetch(
+      'https://raw.githubusercontent.com/Neyen108/spotify-world-charts/main/data/spotifyWorldCharts.json'
+    ).then((res) => res.json());
+  });
+
+  return <>{isLoading ? <Loader /> : <World countries={data} />}</>;
 }
 
 export default App;
