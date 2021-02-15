f24c629482a9ed62794b304c9b08a311e1da2432
Nayanjeet Saikia <65288687+Neyen108@users.noreply.github.com>
2021-02-15 23:03:03 +0530

    add styles

--- src/TopTracks.js
+++ src/TopTracks.js
@@ -26,59 +26,68 @@ const TopTracks = (props) => {
   } else {
     return (
       <>
-        <div>
-          {tracksData.map((track, i) => {
-            const imgSrc = track.album.images[1].url;
-            const trackName = track.name;
-            const artistName = track.artists[0].name;
-            const album = track.album.name;
-            const albumType = track.album.album_type;
-            const previewUrl = track.preview_url;
-            const spotifyTrackUrl = track.external_urls.spotify;
-            const albumUrl = track.album.external_urls.spotify;
+        <div className='container'>
+          <div className='country'>
+            The Top 10 most streamed songs in{' '}
+            <span>{props.location.state} </span>
+          </div>
+          <table className='chart-table' style={{ display: 'table' }}>
+            <tbody>
+              {tracksData.map((track, i) => {
+                const imgSrc = track.album.images[1].url;
+                const trackName = track.name;
+                const artistName = track.artists[0].name;
+                const album = track.album.name;
+                const albumType = track.album.album_type;
+                const previewUrl = track.preview_url;
+                const spotifyTrackUrl = track.external_urls.spotify;
+                const albumUrl = track.album.external_urls.spotify;
 
-            if (previewUrl) {
-              return (
-                <div className='track-container'>
-                  <img src={imgSrc} alt={trackName} className='track-img' />
-                  <div className='track-details'>
-                    <h4>Track : {trackName}</h4>
-                    <h4>Artist : {artistName}</h4>
-                    <h4>
-                      <a href={albumUrl}>Album : {album}</a>
-                    </h4>
-                    <h4>Album Type : {albumType}</h4>
-                    <h4>Track : {trackName}</h4>
-                    <button className='btn'>Preview</button>
-                    <a href={spotifyTrackUrl}>
-                      Listen to full track on spotify
-                    </a>
-                  </div>
-                </div>
-              );
-            } else {
-              return (
-                <div className='track-container'>
-                  <img src={imgSrc} alt={trackName} className='track-img' />
-                  <div className='track-details'>
-                    <p>Track : {trackName}</p>
-                    <h4>Artist : {artistName}</h4>
-                    <h4>
-                      <a href={albumUrl}>Album : {album}</a>
-                    </h4>
-                    <h4>Album Type : {albumType}</h4>
-                    <h4>Track : {trackName}</h4>
-                    <button className='btn'>No Preview available</button>
-                    <a href={spotifyTrackUrl}>
-                      Listen to full track on spotify
-                    </a>
-                  </div>
-                </div>
-              );
-            }
-          })}
+                if (previewUrl) {
+                  return (
+                    <tr>
+                      <td className='track-img'>
+                        <img src={imgSrc} alt='' />
+                      </td>
+                      <td className='position'>
+                        <span>{i + 1}</span>
+                      </td>
+                      <td className='track-details'>
+                        <span className='track-name'>{trackName}</span>
+                        <br />
+                        <span> by {artistName}</span>
+                      </td>
+                      <td className='utilities'>
+                        <button>Preview</button> <br />
+                        <button>Listen to full track on Spotify</button>
+                      </td>
+                    </tr>
+                  );
+                } else {
+                  return (
+                    <tr>
+                      <td className='track-img'>
+                        <img src={imgSrc} alt='' />
+                      </td>
+                      <td className='position'>
+                        <span>{i + 1}</span>
+                      </td>
+                      <td className='track-details'>
+                        <span className='track-name'>{trackName}</span>
+                        <br />
+                        <span> by {artistName}</span>
+                      </td>
+                      <td className='utilities'>
+                        <button>Preview</button> <br />
+                        <button>Listen to full track on Spotify</button>
+                      </td>
+                    </tr>
+                  );
+                }
+              })}
+            </tbody>
+          </table>
         </div>
-
         <audio />
       </>
     );
