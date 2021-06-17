a330f13114e6eff610b3f2295b9337d6741f8bf1
Nayanjeet Saikia <nayanjeet108@gmail.com>
2021-06-17 07:38:59 +0530

    Update action.js

--- action.js
+++ action.js
@@ -54,10 +54,10 @@ const getCountriesData = async () => {
       data[country.toUpperCase()] = countryData;
 
       urlData[country.toUpperCase()] = countryData.map((item) => { 
-        if(item.url){
+        
         const lastItem = item.url.substring(item.url.lastIndexOf('/') + 1);
         return lastItem;
-        }
+        
       });
     }
   }
